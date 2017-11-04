8e4274800c56f76ca32d2f4ca57a6f79e0fa55a0
LAPTOP-JCFJCJ8H\acer <githubprivate@roguish.com>
2017-11-04 15:53:39 -0700

    cannon rotation in progress

--- Assets/Flyer.cs
+++ Assets/Flyer.cs
@@ -7,12 +7,9 @@ public class Flyer : MonoBehaviour {
     private Rigidbody _rb;
     private Quaternion _originalRotation;
     private Vector3 _originalPosition;
-    public float powerCharge = 1;
 
-    public float firePowerMultiplier = 1;
-    public float firePowerIncrementer = 0.001f;
-    public float firePowerReset = 1;
-    public float firePowerMax = 3;
+
+
 
     // Use this for initialization
     void Start () {
@@ -21,45 +18,19 @@ public class Flyer : MonoBehaviour {
         _originalRotation = transform.rotation;
     }
 	
-	// Update is called once per frame
 	void Update () {
-
-        // FirePower
-        if (Input.GetKey("space"))
-        {
-            resetFlyer();
-
-            powerCharge += firePowerIncrementer;
-            if( powerCharge > firePowerMax )
-            {
-                powerCharge = firePowerMax;
-            }
-        }
-        
-        // Fire
-        if (Input.GetKeyUp("space"))
-        {
-            fireProjectile();
-        }
-
-        // Orientation
-      
+        // empty
     }
 
-    public void fireProjectile()
+    public void fireProjectile( Vector3 pFireVector )
     {
         Debug.Log("Fire!");
-        _rb.AddForce(transform.up * firePowerMultiplier * powerCharge, ForceMode.Impulse);
-        resetPower();
+        _rb.AddForce( pFireVector, ForceMode.Impulse );
     }
 
-    public void resetPower()
+    public void resetFlyerTransform()
     {
-        powerCharge = firePowerReset;
-    }
-
-    public void resetFlyer()
-    {
+        // position reset
         transform.position = _originalPosition;
         transform.rotation = _originalRotation;
     }
---
+++ Assets/GameController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject cannon;
+    public GameObject flyer;
+    public float firePowerReset = 1;
+    public float firePowerMax = 3;
+    public float firePowerMultiplier = 1;
+    public float firePowerIncrementer = 0.001f;
+    public float cannonPitch;
+    public float cannonPitchMin = 0;
+    public float cannonPitchMax = 180;
+
+    private Quaternion _cannonRotationOriginal;
+    private float _powerCharge = 1;
+    private float _cannonPitch = 0;
+   
+    void Start () {
+        _cannonRotationOriginal = cannon.transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _cannonPitch += Input.GetAxis("Vertical");
+        setCannonPitch(_cannonPitch);
+
+        cannon.transform.rotation = Quaternion.Euler(cannonPitch, 180f, 0);
+
+        // FirePower
+        if (Input.GetKeyDown("z"))
+        {
+            resetLaunchpad();
+        }
+
+        if (Input.GetKey("z"))
+        {
+            increaseFirePower();
+
+            // set cannon firepower indicator by power
+            cannon.GetComponent<Cannon>().setFirepowerIndicatorPositionByPower( _powerCharge );
+        }
+
+        // Fire
+        if (Input.GetKeyUp("z"))
+        {
+            // rotate the flyer -90 so it flies head first
+            Vector3 tFireVector = new Vector3( _cannonPitch - 90f, 0, 0 ) * _powerCharge;
+
+            flyer.GetComponent<Flyer>().fireProjectile( tFireVector );
+        }
+    }
+
+    public void increaseFirePower()
+    {
+        _powerCharge += firePowerIncrementer;
+        if (_powerCharge > firePowerMax)
+        {
+            _powerCharge = firePowerMax;
+        }
+    }
+
+    public void resetLaunchpad()
+    {
+        flyer.GetComponent<Flyer>().resetFlyerTransform();
+        
+        // firepower reset
+        resetPower();
+
+        // cannon reset
+        cannon.GetComponent<Cannon>().resetFirepowerIndicator();
+    }
+
+    public void resetPower()
+    {
+        _powerCharge = firePowerReset;
+    }
+
+    // Cannon
+    public void setCannonPitch(float pPitch)
+    {
+        cannonPitch = Mathf.Max(Mathf.Min(cannonPitchMax, pPitch), cannonPitchMin);
+    }
+}
